cc18db04198aa65924357945ae46e0210a70ee89
Isaac Overacker <ioveracker@cbtnuggets.com>
2017-01-08 17:36:23 -0800

    Initial project setup.
    - Bundler
    - Cocoapods
    - Xcode Project
    - Swiftlint

---
+++ GarageTests/GarageTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import Garage
+
+class GarageTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {
+    }
+
+    func testPerformanceExample() {
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+
+target 'Garage' do
+  use_frameworks!
+
+  pod 'WebIOPi', :path => '../WebIOPi'
+  target 'GarageTests' do
+    inherit! :search_paths
+    pod 'Quick'
+    pod 'Nimble'
+  end
+
+end
